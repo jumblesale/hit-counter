8b49ffd50f4c22d8a0db9a0c9d5d605e78790ab7
jumblesale <rob@robsollars.co.uk>
2017-03-22 14:43:08 +0000

    made it replace text on the web page

--- hit-counter.py
+++ hit-counter.py
@@ -2,9 +2,11 @@ import argparse
 import os.path
 import sys
 import inotify.adapters
+from functools import partial
 
 
 LOGGING_ENABLED = True
+REPLACE_LOCATION = '<!-- HIT_COUNTER -->'
 
 
 def main():
@@ -19,13 +21,13 @@ def main():
     count_file = args.countfile
     if validate_arguments(page, count_file) is False:
         sys.exit(-1)
-    watch(page, on_file_read)
+    action = partial(on_file_read, count_file, page)
+    watch(page, action)
 
 
 def watch(page, action):
     dir = str.encode(get_directory_of_file(page))
     watched_page = page.split('/')[-1]
-    log(dir)
     i = inotify.adapters.Inotify()
 
     i.add_watch(dir)
@@ -40,15 +42,38 @@ def watch(page, action):
         i.remove_watch(dir)
 
 
-def on_file_read():
-    log('file was read')
+def on_file_read(count_file, html_page):
+    count = update_count_file(count_file)
+    update_html(html_page, count)
+
+
+def update_count_file(path):
+    with open(path, 'r') as f:
+        try:
+            existing = int(f.read())
+        except ValueError:
+            existing = 0
+    with open(path, 'w') as f:
+        f.write(str(existing + 1))
+    return existing + 1
+
+
+def update_html(path, count):
+    with open(path, 'r') as f:
+        existing = f.read()
+    new_page = existing.split("\n")
+    for i, line in enumerate(new_page):
+        if line.startswith(REPLACE_LOCATION):
+            new_page[i] = "{0} {1}".format(REPLACE_LOCATION, str(count))
+    with open(path, 'w') as f:
+        f.write("\n".join(new_page))
 
 
 def validate_arguments(page, count_file):
     if file_can_be_accessed(page, 'r') is False:
         print("{0} could not be opened for reading - are you sure it's the right file?".format(page))
         return False
-    if file_can_be_accessed(count_file, 'w+') is False:
+    if file_can_be_accessed(count_file, 'a+') is False:
         print("{0} could not be opened for writing - are you sure it's the right file?".format(count_file))
         return False
     return True
@@ -72,4 +97,7 @@ def log(msg):
 
 
 if __name__ == "__main__":
-    main()
+    count_file = './count_file'
+    page = './page.html'
+    action = partial(on_file_read, count_file, page)
+    action()
